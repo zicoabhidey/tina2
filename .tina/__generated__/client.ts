0c5c43ecbd425e89cd578435ddaf34bde893034c
zicoabhidey <dey.zico@gmail.com>
2025-07-30 09:38:19 -0400

    Fix Tina GitHub Pages deployment

--- .tina/__generated__/client.ts
+++ .tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ cacheDir: '/home/zico/tina2/.tina/__generated__/.cache/1753882675864', url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
 export default client;
   
--- next.config.js
+++ next.config.js
@@ -11,6 +11,12 @@ const nextConfig = {
   },
   basePath: isProd ? '/tina2' : '',
   assetPrefix: isProd ? '/tina2/' : '',
+  // Add this to fix routing issues
+  async rewrites() {
+    return []
+  },
+  // Ensure proper static export
+  distDir: 'out'
 }
 
 module.exports = nextConfig
